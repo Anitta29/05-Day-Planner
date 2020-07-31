a33df6d6c555da2a7183d036954a1aeee2b873a1
Anita <dananiuta@gmail.com>
2020-07-31 14:47:55 -0400

    saving to git

---
+++ script.js
@@ -0,0 +1,23 @@
+var time = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+
+
+  
+
+
+
+
+
+
+
+// $.each(time, function (index, value) {
+//     var inputGroup = $('<div class="input-group mb-3" style="width:50%"><div class="input-group-prepend"><span class="input-group-text"></span></div><input type="text" class="form-control" aria-label="Amount (to the nearest dollar)"><button>Save</button></div>')
+//     $(".container").append(inputGroup)
+
+//   });
+//   $("button").on("click", function() {
+//     console.log("button-click")
+// var elem = $(this)
+//     console.log(elem)
+//     elem
+    
+//      })
