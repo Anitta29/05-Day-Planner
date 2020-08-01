fcbf881bfd9e9782f57baf501fde915d26c089bd
Anita Dan <anita@Anitas-MacBook-Air.local>
2020-07-31 23:25:58 -0400

    updated planner

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-var time = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+$(document).ready(function() {
+
+$(".saveBtn").on("click", function() {
+
+    // Grab users input 
+var value = $(this).siblings(".reservation").val();
+ //grab the corresponding time
+var time = $(this).parent().attr("id")
 
 
-  
+
+localStorage.setItem(time, value);
+});
 
 
 
 
+// Grab hr and reservation value from local storage and put to page
+$("#hr9 .reservation").val(localStorage.getItem("hr9"))
+$("#hr10 .reservation").val(localStorage.getItem("hr10"))
+$("#hr11 .reservation").val(localStorage.getItem("hr11"))
+$("#hr12 .reservation").val(localStorage.getItem("hr12"))
+$("#hr1 .reservation").val(localStorage.getItem("hr1"))
+$("#hr2 .reservation").val(localStorage.getItem("hr2"))
+$("#hr3 .reservation").val(localStorage.getItem("hr3"))
+$("#hr4 .reservation").val(localStorage.getItem("hr4"))
+$("#hr5 .reservation").val(localStorage.getItem("hr5"))
+
+
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 
 
-// $.each(time, function (index, value) {
-//     var inputGroup = $('<div class="input-group mb-3" style="width:50%"><div class="input-group-prepend"><span class="input-group-text"></span></div><input type="text" class="form-control" aria-label="Amount (to the nearest dollar)"><button>Save</button></div>')
-//     $(".container").append(inputGroup)
-
-//   });
-//   $("button").on("click", function() {
-//     console.log("button-click")
-// var elem = $(this)
-//     console.log(elem)
-//     elem
-    
-//      })
+})
